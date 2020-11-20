fe5b712bf77d882822a84fbb6d5820c34b0793de
Jaelson R Cunha <jaelsonrc@hotmail.com>
2020-11-20 17:09:12 +0100

    front-end

---
+++ ServiceCommission/wwwroot/precache-manifest.d41fb0dd581933736f095c7f5a757028.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4f21f67a609a1642e466b678599cda8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "92761a458a8543298045",
+    "url": "./static/css/2.08a81a33.chunk.css"
+  },
+  {
+    "revision": "92761a458a8543298045",
+    "url": "./static/js/2.85291599.chunk.js"
+  },
+  {
+    "revision": "64f7e58d0b20d4d7a9a6188b47824605",
+    "url": "./static/js/2.85291599.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11c849de3d205791bba1",
+    "url": "./static/js/main.5a63005f.chunk.js"
+  },
+  {
+    "revision": "de27b9fd713b4e769b94",
+    "url": "./static/js/runtime-main.0f6afe03.js"
+  },
+  {
+    "revision": "b258ec363a342fce0cb9369068d73169",
+    "url": "./static/media/LogoFafa.b258ec36.png"
+  }
+]);
